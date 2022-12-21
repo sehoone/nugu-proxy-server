1a3e304efd428f1e362ce07b5033b741f5f02c27
sehoon <sehoon@openobject.net>
2022-12-21 17:26:45 +0900

    add-logging-filter

--- src/main/java/net/openobject/nuguproxyserver/config/WebConfig.java
+++ src/main/java/net/openobject/nuguproxyserver/config/WebConfig.java
@@ -1,8 +1,6 @@
 package net.openobject.nuguproxyserver.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 /**
  * web 설정
@@ -14,15 +12,15 @@ public class WebConfig{
 	 * logging filter. 
 	 * 로그에 노출할 정보를 설정한다
 	 */
-	@Bean
-	public CommonsRequestLoggingFilter requestLoggingFilter() {
-		CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
-		loggingFilter.setIncludeClientInfo(true); // logging include client info
-		loggingFilter.setIncludeHeaders(true); // include header
-		loggingFilter.setIncludeQueryString(true); // include querystring
-		loggingFilter.setIncludePayload(true);	// include body
-		loggingFilter.setMaxPayloadLength(64000); // include body size
-		return loggingFilter;
-	}
+	// @Bean
+	// public CommonsRequestLoggingFilter requestLoggingFilter() {
+	// 	CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+	// 	loggingFilter.setIncludeClientInfo(true); // logging include client info
+	// 	loggingFilter.setIncludeHeaders(true); // include header
+	// 	loggingFilter.setIncludeQueryString(true); // include querystring
+	// 	loggingFilter.setIncludePayload(true);	// include body
+	// 	loggingFilter.setMaxPayloadLength(64000); // include body size
+	// 	return loggingFilter;
+	// }
 
 }
